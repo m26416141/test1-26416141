04bee4f6ca9129ba238726d51c1ba6ed340bc44c
m26416141 <m26416141@john.petra.ac.id>
2021-03-30 20:21:58 +0800

    tab 2 add photo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/core';
+
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoService: PhotoService) {}
 
+  async addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    this.photoService.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
+  }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 }
